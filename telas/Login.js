05c99fe8736050b16c3aeb00e8319f1149739eef
PedroMF <pedrolucas3049@gmail.com>
2023-11-04 20:05:39 -0300

    tela login atualizada

--- telas/Login.js
+++ telas/Login.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useState } from 'react'
+import { Alert, StyleSheet, TextInput, View } from 'react-native'
 import { Button, Text } from 'react-native-paper'
 
 const Login = ({ navigation }) => {
+
+  const [idUser, setUser] = useState('')
+
+   async function salvarid(){
+    const user = {
+      idUser
+    }
+
+    await AsyncStorage.setItem("Usuario", JSON.stringify(user));
+    console.log(user)
+  }
+
+  //  async function mostrar(){
+  //   const json = await AsyncStorage.getItem("user");
+  //   const mostrarUser = JSON.parse(json);
+
+  //   alert("O id de Usuario é: ", mostrarUser);
+  // }
+
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}></Text>
         <TextInput
+        value={idUser}
           style={styles.input}
           placeholder="ID do usuário"
+          onChangeText={text => setUser(text)}
         />
-        <Button mode='outlined' onPress={() => navigation.push('principal-bottom-nav')}>Entrar</Button>
+        <Button mode='outlined' onPress={salvarid()}>Entrar</Button>
+        {/* <Button mode='outlined' onPress={mostrar()}>Alertar</Button> */}
       </View>
     </>
   )
